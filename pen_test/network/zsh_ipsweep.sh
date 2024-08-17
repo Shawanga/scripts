681edf53b165be19871a4c0fffa69f4f92635a0b
Yana Nesterenko <nesterenkoyana@yahoo.com>
2024-08-17 11:10:37 -0400

    syntax update for subnet

--- pen_test/network/zsh_ipsweep.sh
+++ pen_test/network/zsh_ipsweep.sh
@@ -1,11 +1,12 @@
 #!/bin/zsh
 
 if [ -z "$1" ]; then 
-    echo "Add IP network prefix."
-    echo "Syntax: ./zsh_ipsweep.sh xxx.xxx.xxx"
+    echo "Add IP network subnet (e.g., xxx.xxx.xxx.0/24)."
+    echo "Syntax: ./zsh_ipsweep.sh xxx.xxx.xxx.0/24"
 else
+    subnet=$(echo $1 | cut -d '/' -f 1 | sed 's/\.[0-9]*$//')
     for ip in {1..254}; do
-        ping -c 1 $1.$ip | grep "64 bytes" | awk '{print $4}' | tr -d ":" &
+        ping -c 1 $subnet.$ip | grep "64 bytes" | awk '{print $4}' | tr -d ":" &
     done
     wait
 fi
