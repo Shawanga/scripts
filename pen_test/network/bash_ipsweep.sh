bbc472899b1ef91460e894aa0089abf6c67a3abc
Yana Nesterenko <nesterenkoyana@yahoo.com>
2024-08-18 15:23:26 -0400

    updated to ipsweep to scope subnet

--- pen_test/network/bash_ipsweep.sh
+++ pen_test/network/bash_ipsweep.sh
@@ -1,11 +1,30 @@
-#!/bin/bash 
+#!/bin/bash
+# Ip-sweep script to scan only given subnet
+# Prerequisite: bash
+# equivalent to nmap -sn x.x.x.x/24
 
 if [ "$1" == "" ]; then 
-    echo "Add IP network prefix."
-    echo "Syntax: ./bash_ipsweep.sh xxx.xxx.xxx"
+    echo "Add IP network subnet (e.g., 192.168.0/24)."
+    echo "Syntax: ./bash_ipsweep.sh x.x.x.x/x"
 else
-    for ip in $(seq 1 254); do
-        ping -c 1 $1.$ip | grep "64 bytes" | cut -d " " -f 4 | tr -d ":" &
+    # calculating network range with ipcalc
+    network=$(ipcalc -n $1 | grep Network | awk '{print $2}')
+    start_ip=$(ipcalc -b $1 | grep HostMin | awk '{print $2}')
+    end_ip=$(ipcalc -b $1 | grep HostMax | awk '{print $2}')
+
+    # IP octets to integers
+    IFS=. read -r s1 s2 s3 s4 <<< "$start_ip"
+    IFS=. read -r e1 e2 e3 e4 <<< "$end_ip"
+
+    for ((a=s1; a<=e1; a++)); do
+        for ((b=s2; b<=e2; b++)); do
+            for ((c=s3; c<=e3; c++)); do
+                for ((d=s4; d<=e4; d++)); do
+                    ip="$a.$b.$c.$d"
+                    ping -c 1 $ip | grep "64 bytes" | cut -d " " -f 4 | tr -d ":" &
+                done
+            done
+        done
     done
-    wait
+    wait # wait gifor all background jobs to finish
 fi
