faa7921ff6324421a7b5e3c6463a88f6e72dc315
Yana Nesterenko <nesterenkoyana@yahoo.com>
2024-08-18 10:44:27 -0400

    updated to ipsweep only scope subnet

--- pen_test/network/zsh_ipsweep.sh
+++ pen_test/network/zsh_ipsweep.sh
@@ -1,12 +1,30 @@
 #!/bin/zsh
+# Ip-sweep script to scan only given subnet
+# Prerequisite: zsh, ipcalc (brew install ipcalc)
+# equivalnent to nmap -sn x.x.x.x/24
 
 if [ -z "$1" ]; then 
-    echo "Add IP network subnet (e.g., xxx.xxx.xxx.0/24)."
-    echo "Syntax: ./zsh_ipsweep.sh xxx.xxx.xxx.0/24"
+    echo "Add IP network subnet (e.g., 192.168.1.0/24)."
+    echo "Syntax: ./zsh_ipsweep.sh 192.168.1.0/24"
 else
-    subnet=$(echo $1 | cut -d '/' -f 1 | sed 's/\.[0-9]*$//')
-    for ip in {1..254}; do
-        ping -c 1 $subnet.$ip | grep "64 bytes" | awk '{print $4}' | tr -d ":" &
+    # calculating network range with ipcalc
+    network=$(ipcalc -n $1 | grep Network | awk '{print $2}')
+    start_ip=$(ipcalc -b $1 | grep HostMin | awk '{print $2}')
+    end_ip=$(ipcalc -b $1 | grep HostMax | awk '{print $2}')
+
+    # IP octets to integers
+    IFS=. read -r s1 s2 s3 s4 <<< "$start_ip"
+    IFS=. read -r e1 e2 e3 e4 <<< "$end_ip"
+
+    for ((a=s1; a<=e1; a++)); do
+        for ((b=s2; b<=e2; b++)); do
+            for ((c=s3; c<=e3; c++)); do
+                for ((d=s4; d<=e4; d++)); do
+                    ip="$a.$b.$c.$d"
+                    ping -c 1 $ip | grep "64 bytes" | awk '{print $4}' | tr -d ":" &
+                done
+            done
+        done
     done
-    wait
+    wait # wait for all background jobs to finish
 fi
