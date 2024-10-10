dd7d69745a8d4c62ca006aadae7b0fbdb6b7e497
nesterenkoyana@yahoo.com <nesterenkoyana@yahoo.com>
2024-10-10 14:40:22 -0400

    add evaluation for range in addition to explicit rule

--- cis/cis_Ubuntu_22.04_benchmarks.sh
+++ cis/cis_Ubuntu_22.04_benchmarks.sh
@@ -6,5 +6,22 @@
 
 ufw_out="$(ufw status verbose)"
 ss -tuln | awk '($5!~/%lo:/ && $5!~/127.0.0.1:/ && $5!~/::1/) {split($5, a, ":"); print a[2]}' | sort | uniq | while read -r lpn; do
-   ! grep -Pq "^\h*$lpn\b" <<< "$ufw_out" && echo "- Port: \"$lpn\" is missing a firewall rule"
+   # if explicit rule
+   if ! grep -Pq "^\h*(?:[^\s]+\s+){3}(?:${lpn}\b|(\d+):(\d+))" <<< "${ufw_out}"; then
+        # if range rule
+        while read -r rule; do
+            start_port=$(echo "${rule}" | awk '{split($1, range, ":"); print range[1]}')
+            end_port=$(echo "${rule}" | awk '{split($1, range, ":"); print range[2]}')
+
+            if [[ ${lpn} -ge ${start_port} && ${lpn} -le ${end_port} ]]; then
+                covered=true
+                break
+            fi
+    done < <(echo "${ufw_out}" | grep -P "^\h*(?:[^\s]+\s+){3}(\d+):(\d+)")
+
+
+        if [[ "${covered}" != true ]]; then
+            echo "- Port: \"${lpn}\" is missing a firewall rule"
+        fi
+    fi
 done
